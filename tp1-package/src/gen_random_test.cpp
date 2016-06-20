c82ad537955ec950efe4bd997f803ecdba435bb3
Lucas Tavolaro Ortiz <tavo92@gmail.com>
2016-06-20 18:07:07 -0300

    Creado script para generar un caso random

---
+++ tp1-package/src/gen_random_test.cpp
@@ -0,0 +1,37 @@
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+using namespace std;
+
+const int N = 15;
+const int K = 30;
+const int MAX_GOLES_A = 40;
+const int MAX_GOLES_B = 40;
+
+int main(int argc, char * argv[]){
+    freopen("random_test.in", "w", stdout);
+    printf("%i %i\n", N, K);
+
+    srand(time(NULL));
+    for (int k = 0; k<K; ++k) {
+        int equipA, equipB;
+        do {
+            equipA = rand()%N + 1;
+            equipB = rand()%N + 1;
+        } while (equipA == equipB);
+
+        int golesA = rand()%MAX_GOLES_A + 1, golesB = rand()%MAX_GOLES_B + 1;
+        if (golesA == golesB) {
+            if (rand()%2 == 0) {
+                golesA++;
+            } else {
+                golesB++;
+            }
+        }
+
+        printf("0 %i %i %i %i\n", equipA, golesA, equipB, golesB);
+    }
+
+    return 0;
+}
